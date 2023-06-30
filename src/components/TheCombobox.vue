6fb3fa9959ddee1df3a1be9961909f2f8aa3cbfa
konkarin <konkarin.photo@gmail.com>
2023-06-30 11:42:06 +0900

    feat: 🎸 add keyboard operations

--- src/components/TheCombobox.vue
+++ src/components/TheCombobox.vue
@@ -1,16 +1,67 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue'
+
 const inputValue = ref('')
+
 const options = ref(['hoge', 'fuga', 'piyo'])
 const filteredOptions = computed(() => {
   if (!inputValue.value) return options.value
+
   return options.value.filter((option) => {
     return option.includes(inputValue.value)
   })
 })
+
 const expandedListbox = ref(false)
 const toggleListbox = (value: boolean) => {
   expandedListbox.value = value
+}
+
+const currentSelectedIndex = ref(-1)
+const onKeydownUp = () => {
+  if (currentSelectedIndex.value === -1) {
+    toggleListbox(true)
+  }
+
+  if (currentSelectedIndex.value <= 0) {
+    currentSelectedIndex.value = filteredOptions.value.length - 1
+  } else {
+    currentSelectedIndex.value += -1
+  }
+}
+const onKeydownDown = () => {
+  if (currentSelectedIndex.value === -1) {
+    toggleListbox(true)
+  }
+
+  if (currentSelectedIndex.value >= filteredOptions.value.length - 1) {
+    currentSelectedIndex.value = 0
+  } else {
+    currentSelectedIndex.value += 1
+  }
+}
+
+const onKeydownEnter = () => {
+  if (filteredOptions.value[currentSelectedIndex.value] === undefined) {
+    return
+  }
+
+  inputValue.value = filteredOptions.value[currentSelectedIndex.value]
+  toggleListbox(false)
+  resetCurrentIndex()
+}
+
+const onKeydownEsc = () => {
+  toggleListbox(false)
+  resetCurrentIndex()
+}
+
+const isActive = (index: number) => {
+  return currentSelectedIndex.value === index
+}
+
+const resetCurrentIndex = () => {
+  currentSelectedIndex.value = -1
 }
 </script>
 
@@ -24,10 +75,22 @@ const toggleListbox = (value: boolean) => {
       class="combobox-input"
       @focus="toggleListbox(true)"
       @blur="toggleListbox(false)"
+      @input="resetCurrentIndex()"
+      @keydown.prevent.up="onKeydownUp()"
+      @keydown.prevent.down="onKeydownDown()"
+      @keydown.prevent.enter="onKeydownEnter()"
+      @keydown.prevent.esc="onKeydownEsc()"
     />
     <!-- popup -->
     <div v-if="expandedListbox" class="combobox-listbox">
-      <button v-for="option in filteredOptions" :key="option" class="combobox-option">
+      <button
+        v-for="(option, index) in filteredOptions"
+        :key="option"
+        class="combobox-option"
+        :class="{ 'combobox-option-active': isActive(index) }"
+        @mouseover="currentSelectedIndex = index"
+        @mouseleave="resetCurrentIndex()"
+      >
         {{ option }}
       </button>
     </div>
@@ -40,9 +103,11 @@ const toggleListbox = (value: boolean) => {
   display: grid;
   width: 300px;
 }
+
 .combobox-input {
   padding: 8px 12px;
 }
+
 .combobox-listbox {
   display: grid;
   position: absolute;
@@ -51,10 +116,16 @@ const toggleListbox = (value: boolean) => {
   border: 1px solid #eee;
   padding: 0;
 }
+
 .combobox-option {
   text-align: left;
   padding: 8px 12px;
 }
+
+.combobox-option-active {
+  background-color: #009ae0;
+}
+
 button {
   background: none;
   border: none;
